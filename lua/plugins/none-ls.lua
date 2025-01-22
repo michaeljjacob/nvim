54e7f02461f0d0576ab2b834fb949e203ed3c3a0
Michael Jacob <michaeljjacob0@gmail.com>
2025-01-21 16:11:15 -0800

    spelling working

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,19 +1,36 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-
-				require("none-ls.diagnostics.eslint_d"),
-			},
-		})
-
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+  {
+    "jay-babu/mason-null-ls.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "nvimtools/none-ls.nvim",
+    },
+    opts = {
+      ensure_installed = {
+        "prettier",
+        "stylua",
+      },
+    },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+      "davidmh/cspell.nvim",
+    },
+    config = function()
+      local null_ls = require("null-ls")
+      local cspell = require("cspell")
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.prettier,
+          require("none-ls.diagnostics.eslint_d"),
+          cspell.code_actions,
+          cspell.diagnostics,
+        },
+      })
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
+  }
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,24 +3,25 @@ return {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.8',
         dependencies = { 'nvim-lua/plenary.nvim' },
-        opts = {
-            extensions = {
-                ["ui-select"] = {
-                    require("telescope.themes").get_dropdown {
-                    }
-                }
-            }
-        },
         config = function()
-            local builtin = require("telescope.builtin")
+          local telescope = require("telescope")
+          local themes = require("telescope.themes")  
+          local builtin = require("telescope.builtin")
+
+
+            telescope.setup({
+                extensions = {
+                    ["ui-select"] = themes.get_dropdown({})
+                }
+            })
+
+            telescope.load_extension("ui-select")
+
             vim.keymap.set('n', '<C-p>', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
         end
     },
     {
         'nvim-telescope/telescope-ui-select.nvim',
-        config = function()
-            require("telescope").load_extension("ui-select")
-        end
     }
 }
