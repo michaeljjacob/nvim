ce014809e89568d2ef05320284e37cfac3bc9dce
Michael Jacob <michaeljjacob0@gmail.com>
2025-01-23 14:11:42 -0800

    redo tmux config, fix deno fighting ts lsp, add spelling lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -20,14 +20,22 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities();
-
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({
         capabilities = capabilities,
       })
       lspconfig.ts_ls.setup({
         capabilities = capabilities,
+        single_file_support = false,
+        root_dir = function(fname)
+          if (lspconfig.util.root_pattern("deno.json", "deno.jsonc")(fname) ~= nil) then
+            vim.print("not starting ts lsp")
+            return nil
+          end
+          vim.print("starting ts lsp")
+          return lspconfig.util.root_pattern("package.json", "tsconfig.json", ".git")(fname)
+        end,
       })
       lspconfig.jsonls.setup({
         capabilities = capabilities,
@@ -41,16 +49,16 @@ return {
           lint = true,
           unstable = true,
         },
-        root_dir = require 'lspconfig'.util.root_pattern("deno.json", "deno.jsonc"),
+        root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
       })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set('n', '<leader>e', function()
-        vim.diagnostic.open_float(nil, {focus = false});
-      end, {});
+      vim.keymap.set("n", "<leader>e", function()
+        vim.diagnostic.open_float(nil, { focus = false })
+      end, {})
     end,
   },
 }
